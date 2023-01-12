be64c1bd0e68b31a7b246c499a78a4fd65b8ca19
shawn.wang <shawn.wang86b@gmail.com>
2023-01-12 23:40:29 +1100

    feat: add player

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = "./img/town.png";
 
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
 image.onload = () => {
-  c.drawImage(image, -500, -950);
+  c.drawImage(image, -500, -1000);
+  c.drawImage(
+    playerImage,
+    //crop from position X
+    0,
+    //crop from position Y
+    0,
+    //crop width in X
+    playerImage.width / 4,
+    //crop height in Y
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
